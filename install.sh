e79aa6fe36ec5da5dcb0205af7cd74a3c0c7c575
mgrau <matt.grau@gmail.com>
2018-05-10 15:00:15 +0000

    added vim-autopep8 to install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ ln -s ~/.dotfiles/vim ~/.vim
 
 git clone git://github.com/tpope/vim-commentary ~/.vim/bundle/vim-commentary
 git clone git://github.com/tpope/vim-surround ~/.vim/bundle/vim-surround
+git clone git://github.com/tell-k/vim-autopepp8 ~/.vim/bundle/vim-autopep8
