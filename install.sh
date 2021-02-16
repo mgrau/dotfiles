12d94e2a81a64374f5a680b190a0b445f8e68be0
Matt Grau <matt.grau@gmail.com>
2021-02-16 16:22:57 +0100

    add zsh and oh-my-zsh

--- install.sh
+++ install.sh
@@ -5,9 +5,15 @@ mv `dirname $0` ~/.dotfiles
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/zshrc ~/.zshrc
 
 git clone git://github.com/tpope/vim-commentary ~/.vim/bundle/vim-commentary
 git clone git://github.com/tpope/vim-surround ~/.vim/bundle/vim-surround
 git clone git://github.com/tell-k/vim-autopep8 ~/.vim/bundle/vim-autopep8
 
 ln -s ~/.vim/bundle/ftplugin/python_autopep8.vim ~/.vim/plugin/
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
+
